4ec2de21e42d209f1419f29fb20e4086ecb2edbb
Sajjad <Sajadehsas10@gmail.com>
2025-05-22 14:08:41 +0200

    The product section is done, All data is written now.

---
+++ JerseySweden/src/main/java/com/example/JerseySweden/JerseySwedenApplication.java
@@ -0,0 +1,13 @@
+package com.example.JerseySweden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseySwedenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseySwedenApplication.class, args);
+	}
+
+}
---
+++ JerseySweden/src/test/java/com/example/JerseySweden/JerseySwedenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JerseySweden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseySwedenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
